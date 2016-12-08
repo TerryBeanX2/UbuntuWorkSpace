4635e0814b531baaa03c905cefde24f867378c0e
Terry <462882917@qq.com>
2016-12-08 17:22:09 +0800

    2016/12/8 night

--- Koa/koa.js
+++ Koa/koa.js
@@ -1,10 +1,18 @@
 var koa = require('koa');
 var app = koa();
-
+app.keys = ['im a newer secret','i like you'];
 app.use(function *(){
-    this.body = {
-        
-    }
+    // this.throw(400,'haha stupid');
+    this.body = "hello stupid";
+    console.log(this.request.query);
+    this.cookies.set('name','terry',{
+        signed:true
+    });
+    console.log(this.cookies.get('name'))
+});
+
+app.on('error',function(err,ctx){
+    log.error('server error',err,ctx)
 });
 
 app.listen(3001);
